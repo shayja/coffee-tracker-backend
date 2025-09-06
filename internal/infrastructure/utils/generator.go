46422333cf3fb92aa8d798d879093bad74023620
Shay Jacoby <shayja@gmail.com>
2025-09-06 21:32:26 +0300

    changed method to private

--- internal/infrastructure/utils/generator.go
+++ internal/infrastructure/utils/generator.go
@@ -11,14 +11,14 @@ const charset = "abcdefghijklmnopqrstuvwxyz" +
 var seededRand *rand.Rand = rand.New(
   rand.NewSource(time.Now().UnixNano()))
 
-func GenerateStringWithCharset(length int, charset string) string {
+func GenerateString(length int) string {
+  return generateStringWithCharset(length, charset)
+}
+
+func generateStringWithCharset(length int, charset string) string {
   b := make([]byte, length)
   for i := range b {
     b[i] = charset[seededRand.Intn(len(charset))]
   }
   return string(b)
-}
-
-func GenerateString(length int) string {
-  return GenerateStringWithCharset(length, charset)
 }
